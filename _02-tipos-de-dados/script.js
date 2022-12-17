1cd133f835a5196e68508e39b46c0d2ca5435816
Jhonnathandc <corajhonnathan5@gmail.com>
2022-12-17 17:28:14 -0300

    tipos de dados

---
+++ _02-tipos-de-dados/script.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+var string = 'Isso aqui é uma string';
+console.log(string);
+
+// Declare uma variável contendo um número dentro de uma string
+var numeroString = '22';
+console.log(numeroString);
+
+// Declare uma variável com a sua idade
+var idade = '19';
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Jhonnathan';
+var sobrenome = 'Dias Cora';
+console.log(nome);
+console.log(sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+var verificar = typeof nome;
+console.log(verificar);
