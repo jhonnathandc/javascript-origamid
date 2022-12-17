cb380481c5a0dbb8f1b1b4828af4593e3a4297ec
Jhonnathandc <corajhonnathan5@gmail.com>
2022-12-17 17:17:15 -0300

    variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,25 @@
+// Declarar uma variável com o seu nome
+var nome = "Jhonnathan";
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+var idade = 19;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var cidade;
+var bairro;
+var carro;
+var casa;
+var cor;
+
+console.log(cidade, bairro, carro, casa, cor);
