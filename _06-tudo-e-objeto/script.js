88791dd79dc12a0601a179c901155c386ff30225
Jhonnathandc <corajhonnathan5@gmail.com>
2022-12-18 16:35:32 -0300

    tudo é objeto

---
+++ _06-tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+var string = 'string';
+string.length;
+string.indexOf;
+string.repeat;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+btn.addEventListener;
+btn.appendChild;
+btn.ariaLabel;
+btn.toggleAttribute;
+btn.scrollTop;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
