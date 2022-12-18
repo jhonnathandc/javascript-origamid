5b11c0a5b4ee774286d606a8f5d93ef1cc7097f1
Jhonnathandc <corajhonnathan5@gmail.com>
2022-12-18 20:02:29 -0300

    o que é dom

---
+++ _10-o-que-e-dom/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+var url = window.location.href;
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+var ativo = document.querySelector('.ativo');
+console.log(ativo);
+
+// Retorne a linguagem do navegador
+var linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela 
+var largura = window.innerWidth;
+console.log(largura);
