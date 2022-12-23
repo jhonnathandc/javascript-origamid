177cad7a1dadd2a6abbfbc0a4cab2245d499eaaf
Jhonnathandc <corajhonnathan5@gmail.com>
2022-12-22 22:57:55 -0300

    accordion list

---
+++ _18-accordion-list/js/script.js
@@ -0,0 +1,42 @@
+function initTabMenu() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("active");
+
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove("active");
+      });
+      tabContent[index].classList.add("active");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabMenu();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionList.length) {
+    accordionList[0].classList.add("active");
+    accordionList[0].nextElementSibling.classList.add("active");
+
+    function activeAccordion() {
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("active");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
