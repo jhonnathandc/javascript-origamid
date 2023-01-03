1db7388fd9b1af7576a9bc20cf483125385b1c6e
Jhonnathandc <corajhonnathan5@gmail.com>
2023-01-03 20:48:54 -0300

    Native, Host, User

---
+++ _23-native-host-user/script.js
@@ -0,0 +1,18 @@
+// Liste 5 objetos nativos
+String;
+Number;
+Array;
+Function;
+Object;
+
+// Liste 5 objetos do browser
+Window;
+history;
+Document;
+HTMLCollection;
+NodeList;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+webkitHidden;
+webkitVisibilityState;
