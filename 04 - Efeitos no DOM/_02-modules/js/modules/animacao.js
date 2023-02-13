e8590c7b62196aad3a374bdce50635860e3abdf3
Jhonnathandc <corajhonnathan5@gmail.com>
2023-02-13 19:55:45 -0300

    modules

---
+++ 04 - Efeitos no DOM/_02-modules/js/modules/animacao.js
@@ -0,0 +1,22 @@
+export default function initAnimation() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+  if (sections.length) {
+    const windowHeight = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHeight < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("active");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
---
+++ 04 - Efeitos no DOM/_02-modules/js/script.js
@@ -0,0 +1,9 @@
+import initTabMenu from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js';
+import scrollSuave from './modules/scroll-suave.js';
+import animation from './modules/animacao.js';
+
+initTabMenu();
+initAccordion();
+scrollSuave();
+animation();""
