aa2fc0f55c1b6df2c235ad8bee591b1f6a90a4f9
Jhonnathandc <corajhonnathan5@gmail.com>
2023-02-14 20:08:33 -0300

    tooltip

---
+++ 04 - Efeitos no DOM/_04-tooltip/js/script.js
@@ -0,0 +1,13 @@
+import initTabMenu from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js';
+import scrollSuave from './modules/scroll-suave.js';
+import animation from './modules/animacao.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
+initTabMenu();
+initAccordion();
+scrollSuave();
+animation();
+initModal();
+initTooltip();
