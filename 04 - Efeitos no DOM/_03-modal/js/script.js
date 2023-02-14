2a6361bade4c247a4ba15451218164c2fecb63e2
Jhonnathandc <corajhonnathan5@gmail.com>
2023-02-13 22:07:28 -0300

    modal

---
+++ 04 - Efeitos no DOM/_03-modal/js/script.js
@@ -0,0 +1,11 @@
+import initTabMenu from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js';
+import scrollSuave from './modules/scroll-suave.js';
+import animation from './modules/animacao.js';
+import initModal from './modules/modal.js';
+
+initTabMenu();
+initAccordion();
+scrollSuave();
+animation();
+initModal();
