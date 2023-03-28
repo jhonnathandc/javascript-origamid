d64cd4ef546a343e4575152386e7a62537ef5f18
Jhonnathandc <corajhonnathan5@gmail.com>
2023-03-27 21:11:20 -0300

    date object

---
+++ 04 - Efeitos no DOM/_10-date-object/js/script.js
@@ -0,0 +1,21 @@
+import initTabMenu from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js';
+import scrollSuave from './modules/scroll-suave.js';
+import animation from './modules/animacao.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdown from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/animaNumeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+
+initTabMenu();
+initAccordion();
+scrollSuave();
+animation();
+initModal();
+initTooltip();
+initDropdown();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
