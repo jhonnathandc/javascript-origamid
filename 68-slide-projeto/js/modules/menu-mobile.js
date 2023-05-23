62e5dce22050d31b686811e6cb5b5440c5f57f80
Jhonnathandc <corajhonnathan5@gmail.com>
2023-05-23 00:34:17 -0300

    add slide projeto

---
+++ 68-slide-projeto/js/modules/menu-mobile.js
@@ -0,0 +1,33 @@
+import clickOutside from "./clickoutside.js";
+
+export default class MenuMobile {
+  constructor(buttonMenu, menuList, events) {
+    this.buttonMenu = document.querySelector(buttonMenu);
+    this.menuList = document.querySelector(menuList);
+    this.activeClass = "active";
+
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.buttonMenu.classList.add(this.activeClass);
+    this.menuList.classList.add(this.activeClass);
+
+    clickOutside(this.menuList, this.events, () => {
+      this.buttonMenu.classList.remove(this.activeClass);
+      this.menuList.classList.remove(this.activeClass);
+    });
+  }
+
+  addEventMenuMobile() {
+    this.events.forEach(evento => this.buttonMenu.addEventListener(evento, this.openMenu));
+  }
+
+  init() {
+    this.addEventMenuMobile();
+    return this;
+  }
+}
